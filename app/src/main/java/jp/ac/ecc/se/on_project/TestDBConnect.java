31c5740345189ca9e3396049b189d7cc050810cf
matsusaka-k <matsusaka2220248@gmail.com>
2023-06-26 19:28:50 +0900

    アクティビティ追加

---
+++ app/src/main/java/jp/ac/ecc/se/on_project/TestDBConnect.java
@@ -0,0 +1,14 @@
+package jp.ac.ecc.se.on_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestDBConnect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_dbconnect);
+    }
+}
